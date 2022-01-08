23850a84a0e5ab026a030d1fd9be0a415b9ba3ad
HOSE JOO <hose0728@naver.com>
2022-01-08 19:49:04 +0900

    feature/test

--- src/main/java/MBTI_GROUND/toypj/Config/WebMvcConfig.java
+++ src/main/java/MBTI_GROUND/toypj/Config/WebMvcConfig.java
@@ -13,14 +13,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     //CORS 문제 해결
     registry.addMapping("/**")
         .allowedOrigins("http://localhost:3000")
-        .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+        .allowedMethods("*")
         .allowedHeaders("*")
         .allowCredentials(true) // 내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
         .maxAge(3600);
   }
 
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    WebMvcConfigurer.super.addInterceptors(registry);
-  }
+
 }
