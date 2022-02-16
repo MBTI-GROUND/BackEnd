efe5b5b46dbbfd3a75c2bdf0f6ef52b604f6cfa1
Hose Joo <37391598+hose0728@users.noreply.github.com>
2022-02-17 00:08:27 +0900

    Revert "Feature/chat/hose"

--- src/main/java/MBTI_GROUND/toypj/Repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package MBTI_GROUND.toypj.Repository;
-
-import MBTI_GROUND.toypj.Entity.CommentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<CommentEntity,Long> {
-
-}
--- src/main/java/MBTI_GROUND/toypj/Repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package MBTI_GROUND.toypj.Repository;
-
-import MBTI_GROUND.toypj.Entity.PostEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<PostEntity,Long> {
-
-}
